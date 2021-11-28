7c2b9c9cdf43c2e3b322b7bf7529f09e5056feef
Jiaqing Chen <jchen501@asu.edu>
2021-11-27 19:33:17 -0800

    Clean Up word2vec-inference-exp.py

--- Tensorflow_Test/Text_Classification_Test/word2vec-inference-exp.py
+++ Tensorflow_Test/Text_Classification_Test/word2vec-inference-exp.py
@@ -13,15 +13,11 @@ import tensorflow_hub as hub
 from tensorflow.keras.models import Sequential
 from keras.initializers import Constant
 
-# Comment it if on GPU
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
 SEED = 42
 AUTOTUNE = tf.data.AUTOTUNE
- # Define the vocabulary size and number of words in a sequence.
+# define the vocabulary size and number of words in a sequence.
 vocab_size = 1009375
 embedding_dim = 500
-num_models = 12
 
 def create_word2vec_model(input_weights):
     word2vec = Sequential()
@@ -30,8 +26,15 @@ def create_word2vec_model(input_weights):
     return word2vec
 
 if __name__ == "__main__":
-
-    loading_method  = input("Enter 1 for loading from Postgres OR 2 for loading from CSV file\n")
+    # parse command-line arguments for the number of models for testing
+    if(len(sys.argv)==2):
+        num_models = int(sys.argv[1])
+    else:
+        num_models = 1
+    
+    # ask the user to load input from memory/CSV/Posrgres
+    loading_method  = input("Enter 0 for loading from memory OR 1 for loading from Postgres one column OR 2 for loading from CSV file\n")
+    
     # connect with the postgres
     t_host = "localhost"
     t_port = "5432"
@@ -41,23 +44,28 @@ if __name__ == "__main__":
     db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
     db_cursor = db_conn.cursor()
 
+     # load and save models based on the number of models
     print("loading model")
     word2vec = tf.keras.models.load_model(
     'w2v_wiki500_yelp_embed_nontrainable.h5', custom_objects={'KerasLayer': hub.KerasLayer})
     layer = word2vec.get_layer('keras_layer_1')
     weights= layer.get_weights()
-    weights = np.copy(tf.dtypes.cast(weights, tf.float32))
+    weights = tf.dtypes.cast(weights, tf.float32)
     model_list = []
     for i in range (num_models):
         model_list.append(create_word2vec_model(weights))
     
+    # test the loading time of the inputs
     loading_start = time.time()
-    if(loading_method == "1"):
+    if(loading_method == "0"):
+        print ("loading inputs from Memory")
+        targets = tf.dtypes.cast(np.random.randint(0, vocab_size-1, (1000000,)), tf.float32)
+    elif(loading_method == "1"):
         print ("loading inputs from Postgres")
         # load input from postgres
         try:
             # execute the SELECT statement
-            db_cursor.execute(""" SELECT array_data FROM imdb WHERE id = %s """,(0,))
+            db_cursor.execute(""" SELECT array_data FROM text_G WHERE id = %s """,(0,))
             blob = db_cursor.fetchone()
             targets = np.frombuffer(blob[0])
             # close the communication with the PostgresQL database
@@ -70,14 +78,14 @@ if __name__ == "__main__":
         targets = tf.dtypes.cast(targets, tf.float32)
     elif(loading_method == "2"):
         print ("loading inputs from CSV file")
-        file = open("input_float_G.csv")
+        file = open("input_float.csv")
         targets = np.loadtxt(file, delimiter=",")
-        targets = tf.dtypes.cast(targets, tf.float32)
     else:
         print("Invalid Input")
     loading_end = time.time()
     print('data loading time', loading_end-loading_start, ' seconds')
-
+    
+    # test the inference time
     print("making inference")
     inference_start = time.time()
     for i in range(num_models):
